7546d1f8d122f0c99225e7592d8e0b265f6eab55
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2023-01-20 08:44:21 +0700

    add nav

--- app/src/main/java/com/example/signupretrofit/MainActivity.kt
+++ app/src/main/java/com/example/signupretrofit/MainActivity.kt
@@ -2,14 +2,29 @@ package com.example.signupretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.signupretrofit.databinding.ActivityMainBinding
+import com.example.signupretrofit.fragment.HomeFragment
+import com.example.signupretrofit.fragment.ProfileFragment
+import com.example.signupretrofit.fragment.ShopFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val fragmentHome: Fragment = HomeFragment()
+    val fragmentShop: Fragment = ShopFragment()
+    val fragmentProfile: Fragment = ProfileFragment()
+    val fm: FragmentManager = supportFragmentManager
+    var active: Fragment=fragmentHome
+
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentShop).hide(fragmentShop).commit()
+        fm.beginTransaction().add(R.id.container, fragmentProfile).hide(fragmentProfile).commit()
     }
 }

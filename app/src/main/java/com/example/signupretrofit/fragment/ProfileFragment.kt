f18874a39f3bae691a360f8ceff08a964d96ae86
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2023-01-23 11:21:09 +0700

    Update ProfileFragment.kt

--- app/src/main/java/com/example/signupretrofit/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/signupretrofit/fragment/ProfileFragment.kt
@@ -17,7 +17,7 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val view:View = inflater.inflate(R.layout.fragment_profile, container, false)
 
